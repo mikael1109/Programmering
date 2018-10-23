e093ca6b0cd82418096a61051740b73a98e28d1b
Mikael1109 <mikmen111@gmail.com>
2018-10-23 09:34:54 +0200

    GasTable klar och SolidTable

--- PhysicsLibrary/src/enums/GasTable.java
+++ PhysicsLibrary/src/enums/GasTable.java
@@ -1,5 +1,21 @@
 package enums;
 
-public class GasTable {
+public enum GasTable{
+
+	AIR(1.29, 1.01), 
+	O2(1.43, 0.92), 
+	OXYGEN(1.43, 0.92),
+	CO2(1.98, 0.82), // Koldioxid
+	H2(1.25, 1.04), //Väte
+	He(0.18, 5.1) // Helium
+	;
+
+	double density;
+	double heatCapacity;
+
+	GasTable(double d, double hc) {
+		density = d;
+		heatCapacity = hc * 1E3; // 1E3 = 1*10^3
+	}
 
 }
