7badadbb259e80333ce2c0ddad5aac318d6b93e7
Mikael1109 <mikmen111@gmail.com>
2018-10-23 09:21:04 +0200

    Första commiten. FluidTable och GasTable

---
+++ PhysicsLibrary/src/enums/FluidTable.java
@@ -0,0 +1,27 @@
+package enums;
+
+public enum FluidTable {
+
+	H20(0.998, 4.19, 0, 100, 2260), 
+	WATER(0.998, 4.19, 0, 100, 2260),
+	H2SO4(1.84, 1.38, 10, 336, 511), //Svavelsyra
+	Hg(13.55, 0.14, -39, 357, 296), // Kvicksilver
+	C6H6(0.879, 1.71, 6, 80, 396), // Bensen
+	CH3OH(0.791, 2.50, -98, 65, 1100) // Metanol
+	;
+
+	double density;
+	double heatCapacity;
+	double meltPoint;
+	double boilPoint;
+	double steamEntalpy;
+	
+	FluidTable(double d, double hc, double sp, double bp, double se) {
+		density = d * 1E3;
+		heatCapacity = hc * 1E3;
+		meltPoint = sp;
+		boilPoint = bp;
+		steamEntalpy = se * 1E3;
+	}
+
+}
