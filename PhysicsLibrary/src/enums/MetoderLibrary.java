a89e43520c62ec1ddbbb8a4cee53db33a3a21ea6
Mikael1109 <mikmen111@gmail.com>
2018-10-23 09:53:27 +0200

    Konstanter

---
+++ PhysicsLibrary/src/enums/MetoderLibrary.java
@@ -0,0 +1,11 @@
+package enums;
+
+public class MetoderLibrary 
+{
+	double G = 6.6726 * 1E-11; //Gravitationskonstant
+	double R = 8.3145; //Gaskonstant
+	double p_0 = 1013.25; //Lufttryck vid havsyta
+	double c = 299792458; //Ljusetshastighet i vakum
+	
+		
+}
---
+++ PhysicsLibrary/src/enums/SolidTable.java
@@ -0,0 +1,25 @@
+package enums;
+
+public enum SolidTable {
+
+	ICE(0.92, 2.2, 0, 334),
+	IRON(7.87, 0.45, 1538, 276),
+	COPPER(8.96, 0.39, 1085, 205),
+	GOLD(19.3, 0.13, 1064, 66),
+	SILVER(10.5, 0.24, 962, 105)
+	;
+
+	double density;
+	double heatCapacity;
+	double meltPoint;
+	double meltEntalpy;
+	
+	SolidTable(double d, double hc, double sp, double se) {
+		density = d * 1E3;
+		heatCapacity = hc * 1E3;
+		meltPoint = sp;
+		meltEntalpy = se * 1E3;
+	}
+
+	
+}
