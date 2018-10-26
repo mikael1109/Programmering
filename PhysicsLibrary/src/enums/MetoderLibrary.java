85c89eeee2aaa45087037241d86500a290caf445
Mikael1109 <mikmen111@gmail.com>
2018-10-26 15:18:02 +0200

    La till mer metoder och kommentarer

--- PhysicsLibrary/src/enums/MetoderLibrary.java
+++ PhysicsLibrary/src/enums/MetoderLibrary.java
@@ -2,35 +2,67 @@ package enums;
 
 public class MetoderLibrary 
 {
-	public static double G = 9.8; //Gravitationskonstant
-	public static double R = 8.3145; //Gaskonstant
-	public static double p_0 = 1013.25; //Lufttryck vid havsyta
-	public static double c = 299792458; //Ljusetshastighet i vakum
-	public static double g_swe = 9.824; // Tyngdacceleration Sverige
+	/**
+	 * Sparar värden för Gravitationskonstanten, 
+	 * Almänna gaskonstanten, lufttrycket vid havsytan,
+	 * Ljusets hastighet i vakum och Tyngdaccelerationen för Sverige
+	 */
+	public static double G = 9.8;
+	public static double R = 8.3145; 
+	public static double p_0 = 1013.25;
+	public static double c = 299792458; 
+	public static double g_swe = 9.824; 
 	
-	public static double fahrenheitToCelisius(double fahrenheit) 
+	/**
+	 * Gör om fahrenheit till celsius
+	 * @param fahrenheit tempratur i fahrenheit
+	 * @return skickar tillbaka värdet i celsius
+	 */
+	public static double fahrenheitToCelsius(double fahrenheit)  
 	{
-		fahrenheit = (fahrenheit - 32) / 1.8;
+		fahrenheit = (fahrenheit - 32) / 1.8; 
 		return fahrenheit; 
 	}
 	
-	public static double kelvinToCelisius(double kelvin) {
+	/**
+	 * Gör om kelvin till celsius
+	 * @param kelvin tempratur i kelvin
+	 * @return skickar tillbaka värdet i celsius
+	 */
+	public static double kelvinToCelsius(double kelvin) {
 		kelvin = kelvin - 273.15;
 		return kelvin;
 	}
 	
+	/**
+	 * Tar in vätske densitet och djup och ger vätsketryck
+	 * @param fluid vätskan man ska få ut trycket från 
+	 * @param deep hur djupt ner man ska ta trycket från
+	 * @return skickar tillbaka vätsketryckets värde
+	 */
 	public static double fluidPressure(FluidTable fluid, double deep) {
 		double fluidPressure = 0;
 		fluidPressure = fluid.density * G * deep;
 		return fluidPressure;
 	}
 	
+	/**
+	 * Tar in ett djup och skickar ut trycket i vatten
+	 * @param deep vilket djup det ska va
+	 * @return skickar tillbaka vätsketrycket 
+	 */
 	public static double pressureUnderWater(double deep) {
 		double pressure = 0;
 		pressure = FluidTable.WATER.density * G * deep;
 		return pressure;
 	}
 	
+	/**
+	 * Tar in massan och hastigheten och skickar ut kinetiskt energi värde
+	 * @param mass massan på objektet
+	 * @param velocity objektets hastighet
+	 * @return skickar tillbaka kinetiska energi värdet
+	 */
 	public static double kineticEnergy(double mass, double velocity) 
 	{
 		double ke = 0;
@@ -38,9 +70,44 @@ public class MetoderLibrary
 		return ke;
 	}
 	
+	/**
+	 * Tar in massan och höjden och skickar ut potensiella energi värdet 
+	 * @param mass massan på objektet
+	 * @param height höjden på objektet
+	 * @return skickar tillbaka potensiella energi värdet
+	 */
 	public static double potentialEnergy(double mass, double height) {
 		double pe = 0;
 		pe = mass * G * height;
 		return pe;
+	}
+	
+	/**
+	 * Tar in höjden och skicker ut fallhastighets värdet
+	 * @param height hur långt den faller
+	 * @return skickar tillbaka fallhastighets värdet
+	 */
+	public static double fallSpeed(double height) {
+		double fs = 0;
+		fs = Math.sqrt(2 * G * height);
+		return fs;
+	}
+	
+	/**
+	 * Tar in två värden och skickar ut skillnaden mellan dem
+	 * @param first första värdet
+	 * @param last andra värdet
+	 * @return skickar tillbaka skillnaden mellan värderna
+	 */
+	public static double delta(double first, double last) {
+		double summa = 0;
+		summa = last - first;
+		return summa;
+	}
+	
+	public static double volumeToMass(FluidTable fluid, double volume) {
+		double mass = 0;
+		mass = volume * fluid.density;
+		return mass;
 	}
 }
