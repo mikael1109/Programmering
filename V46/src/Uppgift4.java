ccd264209fe214c13b8f4fbcb8f7c7808438b2d9
Mikael1109 <mikmen111@gmail.com>
2018-11-14 19:05:13 +0100

    Ändrade kommentarer på Uppgift 4 och Uppgift 5

--- V46/src/Uppgift4.java
+++ V46/src/Uppgift4.java
@@ -4,7 +4,7 @@ public class Uppgift4 {
 	public static void main(String[] args) 
 	{
 		int tal = 0;
-		for(int i = 0; i <= 10000; i++) { // Startar loopen, forsätter tills i blir 10000. i lägger en gång i taget
+		for(int i = 0; i <= 10000; i++) { // Loopar tills i når 10 000,
 			if (i%7 == 0) { // Ifall i modulos 7 = 0, ifall inget finns kvar när tar ut delar med 7
 				tal = tal + i; // Plussar på talet som kan delas med sju 
 				System.out.println(i);
--- V46/src/Uppgift5.java
+++ V46/src/Uppgift5.java
@@ -7,10 +7,10 @@ public class Uppgift5 {
 		int steps = 0;
 		System.out.println("Hur många trappsteg ska det vara?");
 		steps = scan.nextInt();
-		for (int i = 0; i < steps; i++) // Start en loop, loopen fortsätter tills "i" = steps lägger till en på "i" varje gång loopen börjar om
+		for (int i = 0; i < steps; i++) // Loopar tills alla steps är utskrivna
 		{
-			for (int y = 0; y <= i; y++) { // Starar en till loop, loopen håller på tills "y" = "i", Lägger till en på "y" varje gång loopen börjar om
-				System.out.print("x"); // Skriver ut x, går inte ner än rad
+			for (int y = 0; y <= i; y++) { // Loopar tills alla x är utskrivna
+				System.out.print("x"); // Skriver ut x
 			}
 			System.out.println(); // Skriver ut ett blankt steg, går ner till nästa rad
 		}
