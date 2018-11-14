e6300c47ad5991e9490c348bae92645feb978b46
Mikael1109 <mikmen111@gmail.com>
2018-11-14 11:04:55 +0100

    Gjort uppgift 3 och kommenterat

---
+++ V46/src/Uppgift3.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Uppgift3 {
+
+	public static void main(String[] args) 
+	{
+		int count = 0;
+		double tal = 1;
+		boolean close = false;
+		Scanner scan = new Scanner(System.in); // Startar scanner
+		System.out.println("Skriv in tal så multipliceras dem");
+		System.out.println("Programet avslutas när talet når 100 000 eller antalet tal når 10");
+		while(!close) { //  Startar loopen medans close är false
+			if(tal <= 100000 && count <= 10) // Ifallen talet är mindre än 100 000 och count är mindre än 10
+			{		
+				tal = tal * scan.nextDouble(); // Gångrar talet med talet som nyss skrevs i konsolen
+				count++; // Lägger till 1 till counten efter varje gång ett tal som skrivs in
+				System.out.println(count + " " + tal);
+				if(count >= 10) { // Ifall count blir lika med 10
+					System.out.println("Du har skrivit 10 tal, talet blev: " + tal);
+					close = true; // Stänger loopen
+				}
+			}else if(tal >= 100000) { // Ifall talet går över 100 000
+				System.out.println("Talet kom över 100 000 det blev: " + tal);
+				close = true; // Stänger loopen
+			}
+		}
+
+	}
+
+}
