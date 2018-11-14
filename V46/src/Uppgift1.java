f1db2622f37639cebf76ba81dd369db7866939c0
Mikael1109 <mikmen111@gmail.com>
2018-11-14 10:31:15 +0100

    Gjorde klart uppgift och la till kommentarer

--- V46/src/Uppgift1.java
+++ V46/src/Uppgift1.java
@@ -13,7 +13,7 @@ public class Uppgift1 {
 			if(nyttal != 0) { //Kollar så att nyttal inte är 0
 				nyttal = scan.nextInt(); // Sätter nyttal till senaste int
 				tal = tal + nyttal; // Plussar på nya talet.
-			}else {
+			}else { // Om nyttal är 0
 				System.out.println(tal); //Skriver ut talet
 				exit = true; // Sätter exit till true
 			}
--- V46/src/Uppgift2.java
+++ V46/src/Uppgift2.java
@@ -3,10 +3,17 @@ public class Uppgift2 {
 
 	public static void main(String[] args) 
 	{
+		boolean close = false;
 		String exit = "EXIT";
-		Scanner scan = new Scanner(System.in);
-		if (scan.nextLine() == exit)
-			
+		String line = "";
+		Scanner scan = new Scanner(System.in); // Startar scanner
+		while(!close) // Loopar medans close är false
+		{
+			line = scan.nextLine(); // Sätter line till senaste scanner line'n
+			if (line.equals(exit)) { // Ifall line = exit stringen
+				close = true; // Sätter close till true, stänger loopen
+			}
+		}
 	}
 
 }
