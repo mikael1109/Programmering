eb36d7caf74e8336fb0efaf5e2c4f714a510ce80
Mikael1109 <mikmen111@gmail.com>
2018-11-14 19:04:07 +0100

    La till kommentarer på uppgift 6

--- V46/src/Uppgift6.java
+++ V46/src/Uppgift6.java
@@ -5,19 +5,19 @@ public class Uppgift6 {
 	{
 		int steps = 0;
 		int spaces = 0;
-		Scanner scan = new Scanner(System.in);
+		Scanner scan = new Scanner(System.in); // Startar scanner
 		System.out.println("Hur många steg ska pyramid gå");
-		steps = scan.nextInt();
-		spaces = steps - 1;
-		for(int i = 0; i < steps; i++) {
-			for(int y = 0; y < spaces; y++) {
-				System.out.print(" ");
+		steps = scan.nextInt(); // Steps till det givna antalet 
+		spaces = steps - 1; // Sätter spaces till steps och tar bort 1
+		for(int i = 0; i < steps; i++) { // Loopar tills alla steps är utskrivna
+			for(int y = 0; y < spaces; y++) { // Loopar tills antalet spaces är utskrivna
+				System.out.print(" "); // Skriver ut en blank ruta
 			}
-			for(int x = 0; x <= i; x++) {
-				System.out.print("x ");
+			for(int x = 0; x <= i; x++) { // Loopar tills antalet x är utskrivna
+				System.out.print("x "); // Skriver ett x och en blank ruta
 			}
-			System.out.println();
-			spaces--;
+			System.out.println(); // Går ned till nästa rad
+			spaces--; // Tar bort en från spaces
 		}
 	}
 
