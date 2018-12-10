33f33dc5a0ba9fe626a79584141e1baabbd374ae
Mikael1109 <mikmen111@gmail.com>
2018-12-10 15:00:23 +0100

    Mer metoder

--- V49/src/EachNumbers.java
+++ V49/src/EachNumbers.java
@@ -9,39 +9,12 @@ public class EachNumbers {
 	
 	public static int[] eNumbers () {
 		int[] list = new int[10];
-
-        int max = 0;
-        int maxNumber = 0;
-
-        int min = 0;
-        int minNumber = 0;
-
+		
         for(int i = 0; i < List.numbers.length; i++)
         {
             list[List.numbers[i]]++;
         }
 
-        for (int i = 0; i < list.length; i++)
-        {
-            if(i == 0)
-            {
-                min = list[i];
-                continue;
-            }
-
-            if(list[i] > max)
-            {
-                max = list[i];
-                maxNumber = i;
-            }
-
-            if(list[i] <= min)
-            {
-                min = list[i - 1];
-                minNumber = i;
-            }
-            
-        }
         return list;
 		}
 }
--- V49/src/mlNumbers.java
+++ V49/src/mlNumbers.java
@@ -2,8 +2,47 @@
 public class mlNumbers {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Det finns flest av " + mlNumbers()[1] + ", " + mlNumbers()[0] + "st och minst av " + mlNumbers()[3] + ", " + mlNumbers()[2] + "st");
 
 	}
+	
+	public static int[] mlNumbers () {
+		int[] list = new int[10];
+		int[] ml = new int[4];
 
+        int max = 0;
+        int ma = 0;
+        int min = 0;
+        int mi = 0;
+
+        for(int i = 0; i < List.numbers.length; i++)
+        {
+            list[List.numbers[i]]++;
+        }
+
+        for (int i = 0; i < list.length; i++)
+        {
+            if(list[i] > max) {
+            	max = list[i];
+            	ma = i;
+            }
+        }
+        min = max;
+        for (int i = 0; i < list.length; i++) 
+        {
+        	if(list[i] < min) {
+        		min = list[i];
+        		mi = i;
+        	}
+        }
+        
+        ml[0] = max;
+        ml[1] = ma;
+        ml[2] = min;
+        ml[3]= mi;
+        return ml;
+		}
 }
+
+
+
