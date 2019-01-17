ef82999dd8abae5daac17fffdd754f8a0c95f6a5
Mikael1109 <mikmen111@gmail.com>
2019-01-17 16:13:28 +0100

    more rooms

--- V2/src/SwitchCase.java
+++ V2/src/SwitchCase.java
@@ -20,15 +20,26 @@ public class SwitchCase {
 			switch(choice) {
 			case 1:
 				System.out.println("The door closes behind you, you are trapped");
-				System.out.println("Restart to continue playing");
 				block();
+				System.out.println("Restart to continue playing");
+				break;
+			case 3:
+				threeExit();
+				System.out.println("1 : Forward  2 : Downward");
+				choice = input.nextInt();
+				switch(choice) {
+				case 1:
+					System.out.println("The room is flooded with water and you drown~");
+					water();
+					System.out.println("Restart to continue playing");
+				}
 			}
 		}
 	}
 	
 	public static void Square() {
 		for(int i = 0; i <= 21; i++) {
-			if(i != 9 && i != 10 && i != 11) {
+			if(i < 8 || i > 12) {
 				System.out.print("-");
 			}else {
 				System.out.print(" ");
@@ -54,7 +65,7 @@ public class SwitchCase {
 	
 	public static void fourExit() {
 		for(int i = 0; i <= 21; i++) {
-			if(i != 9 && i != 10 && i != 11) {
+			if(i < 8 || i > 12) {
 				System.out.print("-");
 			}else {
 				System.out.print(" ");
@@ -76,7 +87,7 @@ public class SwitchCase {
 			System.out.println();
 		}
 		for(int i = 0; i <= 21; i++) {
-			if(i != 9 && i != 10 && i != 11) {
+			if(i < 8 || i > 12) {
 				System.out.print("-");
 			}else {
 				System.out.print(" ");
@@ -104,5 +115,51 @@ public class SwitchCase {
 		for(int i = 0; i <= 21; i++) {
 			System.out.print("-");
 		}
+	}
+	
+	public static void threeExit() {
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		for(int i = 0; i <= 7; i++) {
+			for(int y = 0; y <= 21; y++) {
+				if (y == 0 || y == 21 ) {
+					if(i != 3 && i != 4 || y == 21) {
+						System.out.print("|");
+					}else {
+						System.out.print(" ");
+					}
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
+	public static void water() {
+		System.out.println("----------------------");
+		System.out.println("|             ~      |");
+		System.out.println("|   ~                |");
+		System.out.println("|                    |");
+		System.out.println("|          ~         |");
+		System.out.println("|                 ~  |");
+		System.out.println("|  ~                 |");
+		System.out.println("|                    |");
+		System.out.println("|     ~          ~   |");
+		System.out.println("----------------------");
 	}
 }
