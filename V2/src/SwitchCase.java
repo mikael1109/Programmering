a51b59e8a73f7a18ab665d194c7152ccde5ee753
Mikael1109 <mikmen111@gmail.com>
2019-01-17 19:52:08 +0100

    klar med rum

--- V2/src/SwitchCase.java
+++ V2/src/SwitchCase.java
@@ -3,41 +3,112 @@ import java.util.Scanner;
 public class SwitchCase {
 
 	public static void main(String[] args) {
-		Square();
-		System.out.println("Welcome to the labyrinth, Where would you like to go?");
-		System.out.println("1 : Forward");
+		boolean check = false;
 		Scanner input = new Scanner(System.in);
-		boolean exit = false;
+		square();
+		System.out.println("Welcome to the labyrinth, Where would you like to go?");
+		System.out.println("1 : Up");
 		int choice = input.nextInt();
-
-		String genre;
 
 		switch(choice) {
 		case 1:
 			fourExit();
-			System.out.println("1 : Left  2 : Forward  3 : Right");
+			System.out.println("1 : Left  2 : Up  3 : Right");
 			choice = input.nextInt();
+			check = true;
+			while(check) {
+				
+			}
 			switch(choice) {
 			case 1:
 				System.out.println("The door closes behind you, you are trapped");
 				block();
 				System.out.println("Restart to continue playing");
 				break;
+			case 2:
+				twoExit();
+				System.out.println("1 : Up");
+				choice = input.nextInt();
+				switch(choice) {
+				case 1:
+					System.out.println("There apears to be a tiger in the room");
+					System.out.println("The tiger attacks you and you die~");
+					tiger();
+					System.out.println("Restart to continue playing");
+					break;
+				}
+				break;
 			case 3:
 				threeExit();
-				System.out.println("1 : Forward  2 : Downward");
+				System.out.println("1 : Up  2 : Down");
 				choice = input.nextInt();
 				switch(choice) {
 				case 1:
 					System.out.println("The room is flooded with water and you drown~");
 					water();
 					System.out.println("Restart to continue playing");
+					break;
+				case 2:
+					twoExitSec();
+					System.out.println("1 : Right");
+					choice = input.nextInt();
+					switch(choice) {
+					case 1:
+						threeExitSec();
+						System.out.println("1 : Up  2 : Right");
+						choice = input.nextInt();
+						switch(choice) {
+						case 1:
+							threeExitTri();
+							System.out.println("1 : Up  2 : Right");
+							choice = input.nextInt();
+							switch(choice) {
+							case 1:
+								System.out.println("There apears to be a tiger in the room");
+								System.out.println("The tiger attacks you and you die~");
+								tiger();
+								System.out.println("Restart to continue playing");
+								break;
+							case 2:
+								threeExitSec();
+								System.out.println("1 : Up  2 : Right");
+								choice = input.nextInt();
+								switch(choice) {
+								case 1:
+									twoExit();
+									System.out.println("1 : Up");
+									choice = input.nextInt();
+									switch(choice) {
+									case 1:
+										trophy();
+										System.out.println("You completed the labyrinth. Congratulation!");
+										break;
+									}
+									break;
+								case 2:
+									System.out.println("The door closes behind you, you are trapped");
+									block();
+									System.out.println("Restart to continue playing");
+									break;
+								}
+								break;
+							}
+							break;
+						case 2:
+							System.out.println("The room is flooded with water and you drown~");
+							water();
+							System.out.println("Restart to continue playing");
+							break;
+						}
+					}
+					break;
 				}
+				break;
 			}
 		}
 	}
 	
-	public static void Square() {
+	public static void square() {
 		for(int i = 0; i <= 21; i++) {
 			if(i < 8 || i > 12) {
 				System.out.print("-");
@@ -115,6 +186,7 @@ public class SwitchCase {
 		for(int i = 0; i <= 21; i++) {
 			System.out.print("-");
 		}
+		System.out.println();
 	}
 	
 	public static void threeExit() {
@@ -150,6 +222,129 @@ public class SwitchCase {
 		System.out.println();
 	}
 	
+	public static void twoExit() 
+	{
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		for(int i = 0; i <= 7; i++) {
+			for(int y = 0; y <= 21; y++) {
+				if(y == 0 || y == 21) {
+					System.out.print("|");
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
+	public static void twoExitSec() 
+	{
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		for(int i = 0; i <= 7; i++) {
+			for(int y = 0; y <= 21; y++) {
+				if (y == 0 || y == 21 ) {
+					if(i != 3 && i != 4 || y == 0) {
+						System.out.print("|");
+					}else {
+						System.out.print(" ");
+					}
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= 21; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+	
+	public static void threeExitSec() 
+	{
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		for(int i = 0; i <= 7; i++) {
+			for(int y = 0; y <= 21; y++) {
+				if (y == 0 || y == 21 ) {
+					if(i != 3 && i != 4) {
+						System.out.print("|");
+					}else {
+						System.out.print(" ");
+					}
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= 21; i++) {
+			System.out.print("-");
+		}
+		System.out.println();
+	}
+	
+	public static void threeExitTri(){
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+		for(int i = 0; i <= 7; i++) {
+			for(int y = 0; y <= 21; y++) {
+				if (y == 0 || y == 21 ) {
+					if(i != 3 && i != 4 || y == 0) {
+						System.out.print("|");
+					}else {
+						System.out.print(" ");
+					}
+				}else {
+					System.out.print(" ");
+				}
+			}
+			System.out.println();
+		}
+		for(int i = 0; i <= 21; i++) {
+			if(i < 8 || i > 12) {
+				System.out.print("-");
+			}else {
+				System.out.print(" ");
+			}
+		}
+		System.out.println();
+	}
+	
 	public static void water() {
 		System.out.println("----------------------");
 		System.out.println("|             ~      |");
@@ -161,5 +356,35 @@ public class SwitchCase {
 		System.out.println("|                    |");
 		System.out.println("|     ~          ~   |");
 		System.out.println("----------------------");
+	}
+	
+	public static void tiger() {
+		System.out.println("     (^\\-==-/^)");
+		System.out.println("     >\\ == //<");
+		System.out.println("    :== q''p ==:     _");
+		System.out.println("     .__ qp __.    .' )");
+		System.out.println("      / ^--^ \\    /\\.'");
+		System.out.println("     /_`    / )  '\\/");
+		System.out.println("     (  )  \\  |-'-/");
+		System.out.println("     \\^^,   |-|--'");
+		System.out.println("    ( `'    |_| )");
+		System.out.println("     \\-     |-|/");
+		System.out.println("    (( )^---( ))");
+	}
+	
+	public static void trophy() {
+		for(int i = 0; i <= 13; i++) {
+			System.out.println();
+		}
+		System.out.println("        ___________");
+		System.out.println("       '._==_==_=_.'");
+		System.out.println("       .-\\:      /-.");
+		System.out.println("      | (|:.     |) |");
+		System.out.println("       '-|:.     |-'");
+		System.out.println("         \\::.    /");
+		System.out.println("          '::. .'");
+		System.out.println("            ) (");
+		System.out.println("          _.' '._");
+		System.out.println("         `-------`");
 	}
 }
