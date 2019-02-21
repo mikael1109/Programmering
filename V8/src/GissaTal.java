11f9654019b0ee89f69b0fe7418ce72e7f9bfbac
Mikael1109 <mikmen111@gmail.com>
2019-02-21 13:59:49 +0100

    La till metoder

--- V8/src/GissaTal.java
+++ V8/src/GissaTal.java
@@ -2,55 +2,74 @@ import java.util.Scanner;
 
 public class GissaTal {
 
+	static int tal = 0;
+	static int guess = 0;
+	static int count = 0;
+	static int range = 0;
+	static boolean exit = false;
+	static boolean gissat = false;
+	static Scanner sc = new Scanner(System.in);
+
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int tal = 0;
-		int guess = 0;
-		int count = 0;
-		int range = 0;
-		boolean exit = false;
-		boolean gissat = false;
+		range();
+		do {
+			guess();
+			countRank();
+			restart();
+		} while (!exit);
+
+	}
+
+	public static void range() {
 		System.out.println("Hur mycket ska du gissa mellan 1 - ?");
 		range = sc.nextInt();
 		tal = (int) (Math.random() * range) + 1;
 		System.out.println("Gissa på ett tal mellan 1 till " + range + " fö fan");
-		do {
-			while (!gissat) {
-				guess = sc.nextInt();
-				count++;
-				if (tal == guess) {
-					System.out.println("Nu fick du allt till det, bra jobbat");
-					gissat = true;
-				} else if (guess - tal <= 5 && guess - tal >= -5) {
-					System.out.println("Nu va det riktigt nära gubben");
-				} else if (guess < tal) {
-					System.out.println("Talet är fan större brysh");
-				} else if (guess > tal) {
-					System.out.println("Nu gick du fan över mannen");
-				}
-			}
-			if (count <= 5) {
-				System.out.println("Du gissade: " + count + " gånger, de va la okej");
-			} else if (count >= 12) {
-				System.out.println("Lilla gubben nu tog det allt " + count + " försök, de va ju inte så jävla bra.");
-			} else {
-				System.out.println("Du gissade: " + count + " gånger, fan va sämst");
-			}
-			count = 0;
-			System.out.println("Vill du köra igen. 1: Ja  2: Nej");
+	}
+
+	public static void guess() {
+		while (!gissat) {
 			guess = sc.nextInt();
+			count++;
+			if (tal == guess) {
+				System.out.println("Nu fick du allt till det, bra jobbat");
+				gissat = true;
+			} else if (guess - tal <= 5 && guess - tal >= -5) {
+				System.out.println("Nu va det riktigt nära gubben");
+			} else if (guess < tal) {
+				System.out.println("Talet är fan större brysh");
+			} else if (guess > tal) {
+				System.out.println("Nu gick du fan över mannen");
+			}
+		}
+	}
+
+	public static void countRank() {
+		if (count <= 5) {
+			System.out.println("Du gissade: " + count + " gånger, de va la okej");
+		} else if (count >= 12) {
+			System.out.println("Lilla gubben nu tog det allt " + count + " försök, de va ju inte så jävla bra.");
+		} else {
+			System.out.println("Du gissade: " + count + " gånger, fan va sämst");
+		}
+		count = 0;
+	}
+
+	public static void restart() {
+		System.out.println("Vill du köra igen. 1: Ja  2: Nej");
+		guess = sc.nextInt();
+		while (guess != 1 && guess != 2) {
 			if (guess == 1) {
 				gissat = false;
-				System.out.println("Hur mycket ska du gissa mellan?");
-				range = sc.nextInt();
-				tal = (int) (Math.random() * range) + 1;
-				System.out.println("Gissa på ett tal mellan 1 till " + range + " fö fan");
+				range();
 			} else if (guess == 2) {
 				exit = true;
 				System.out.println("Aja ha det gött för fan");
+			} else {
+				System.out.println("Ogiltlig nummer 1: Ja  2: Nej");
+				guess = sc.nextInt();
 			}
-		} while (!exit);
-
+		}
 	}
 
 }
