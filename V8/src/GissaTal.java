49cb8fcc0b1e0e1d475bc5453e4007af1b1ea485
Mikael1109 <mikmen111@gmail.com>
2019-02-28 13:03:42 +0100

    La till kommentarer

--- V8/src/GissaTal.java
+++ V8/src/GissaTal.java
@@ -6,9 +6,11 @@ public class GissaTal {
 	static int guess = 0;
 	static int count = 0;
 	static int range = 0;
+	static String restart = "";
 	static boolean exit = false;
 	static boolean gissat = false;
-	static Scanner sc = new Scanner(System.in);
+	static Scanner sc = new Scanner(System.in); // Startar scanner
+	
 
 	public static void main(String[] args) {
 		range();
@@ -20,6 +22,9 @@ public class GissaTal {
 
 	}
 
+	/**
+	 * Bestämmer hur mycket du ska gissa mellan, Skriver ut start meddelandet.
+	 */
 	public static void range() {
 		System.out.println("Hur mycket ska du gissa mellan 1 - ?");
 		range = sc.nextInt();
@@ -27,6 +32,10 @@ public class GissaTal {
 		System.out.println("Gissa på ett tal mellan 1 till " + range + " fö fan");
 	}
 
+	/**
+	 * Låter använderen gissa och kollar om gissning var korrekt, ifall inte så skriver den repsektive
+	 * större, mindre eller nära om det var 3 ifrån.
+	 */
 	public static void guess() {
 		while (!gissat) {
 			guess = sc.nextInt();
@@ -44,6 +53,9 @@ public class GissaTal {
 		}
 	}
 
+	/**
+	 * Skriver ut ett meddelande baserat på hur många gånger man har gissat
+	 */
 	public static void countRank() {
 		if (count <= 5) {
 			System.out.println("Du gissade: " + count + " gånger, de va la okej");
@@ -55,21 +67,25 @@ public class GissaTal {
 		count = 0;
 	}
 
+	/**
+	 * Kollar om användaren vill köra igen, ifall ja så startar den en ny runda (range() ) annars
+	 * skriver den ut ett meddelande och stänger av programmet.
+	 */
 	public static void restart() {
-		System.out.println("Vill du köra igen. 1: Ja  2: Nej");
-		guess = sc.nextInt();
+		System.out.println("Vill du köra igen. ja/nej");
+		restart = sc.next();
 		do {
-			if (guess == 1) {
+			if (restart.equals("ja")) {
 				gissat = false;
 				range();
-			} else if (guess == 2) {
+			} else if (restart.equals("nej")) {
 				exit = true;
 				System.out.println("Aja ha det gött för fan");
 			} else {
-				System.out.println("Ogiltlig nummer 1: Ja  2: Nej");
-				guess = sc.nextInt();
+				System.out.println("Ogiltlig svar; ja/nej");
+				restart = sc.next();
 			}
-		}while((guess != 1 && guess != 2));
+		}while(gissat && !exit);
 	}
 
 }
