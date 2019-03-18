68a1550d0b6f801335a6e4f3ec5feb81d8dc97be
Mikael1109 <mikmen111@gmail.com>
2019-03-18 10:31:07 +0100

    La till kommentarer

--- V11/src/SecureDoors.java
+++ V11/src/SecureDoors.java
@@ -4,15 +4,19 @@ public class SecureDoors {
 	
 	static int maxCount = 0;
 	static int count = 0;
-	static Scanner input = new Scanner(System.in);
 	static boolean exit = false;
-	static ArrayList<String> nameList = new ArrayList<>();
+	static Scanner input = new Scanner(System.in); // Skapar Scanner
+	static ArrayList<String> nameList = new ArrayList<>(); // Skapar Arraylisten
+	
 	public static void main(String[] args) 
 	{
-		start();
+		start(); 
 		inputCheck();
 	}
 	
+	/**
+	 * Sätter maxcount alltså hur många inputs man ska ha
+	 */
 	public static void start() {
 		while(maxCount < 1 || maxCount > 1000) {
 		maxCount = input.nextInt();
@@ -21,25 +25,25 @@ public class SecureDoors {
 	
 	public static void inputCheck() {
 		while(!exit) {
-			String[] nameInput = input.nextLine().split(" ");
-			if (nameInput[0].equals("entry") && nameList.contains(nameInput[1])){
+			String[] nameInput = input.nextLine().split(" "); // Splittar stringen vid mellanrummet
+			if (nameInput[0].equals("entry") && nameList.contains(nameInput[1])){ // Kollar ifall entry finns med innan och ifall namnet finns i arraylisten
 				System.out.println(nameInput[1] + " entered (ANOMALY)");
 				count++;
-			}else if(nameInput[0].equals("exit") && !nameList.contains(nameInput[1])) {
+			}else if(nameInput[0].equals("exit") && !nameList.contains(nameInput[1])) { // Kollar ifall exit finns med innan och ifall namnet inte finns med i arraylisten
 				System.out.println(nameInput[1] + " exited (ANOMALY)");
 				count++;
-			}else if(nameInput[0].equals("entry")) {
-				nameList.add(nameInput[1]);
+			}else if(nameInput[0].equals("entry")) { 
+				nameList.add(nameInput[1]); // Lägger till namnet i arraylisten
 				System.out.println(nameInput[1] + " entered");
 				count++;
 			}else if(nameInput[0].equals("exit")) {
-				nameList.remove(nameInput[1]);
+				nameList.remove(nameInput[1]); // Tar bort namnet från arraylisten
 				System.out.println(nameInput[1] + " exited");
-				count++;
+				count++; // Plussar på en på count varje gång en entry eller exit görs
 			}
 			
 			if(count == maxCount) {
-				exit = true;
+				exit = true; // stänger av programmet när maxcount = count
 			}
 		}
 	}
