7785e37afbca0181a147f858a09325645d437cc6
Mikael1109 <mikmen111@gmail.com>
2019-08-28 12:06:57 +0200

    första uppgiften klar

---
+++ Repetition/src/ArraySummerare.java
@@ -0,0 +1,15 @@
+
+public class ArraySummerare {
+
+	public static void main(String[] args) {
+		
+		int[] Nummer = { 4, 3, 9, 7 };
+		int sum = 0;
+		for(int i = 0; i < Nummer.length; i++) {
+			sum += Nummer[i];
+		}
+		sum +=5;
+		System.out.println(sum);
+	}
+
+}
