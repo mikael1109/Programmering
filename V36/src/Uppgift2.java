9dce159cd35dab0f499d8b75b0b2cc287f1b1b7a
Mikael1109 <mikmen111@gmail.com>
2019-09-05 13:15:11 +0200

    uppgift 2 klar med kommentarer

---
+++ V36/src/Uppgift2.java
@@ -0,0 +1,37 @@
+
+public class Uppgift2 {
+
+	static int count = 0; // en int som används för att se om hela ordet är "bakvänt"
+	static String temp = ""; // en temporär string för att placera in char i omvänd ordning
+
+	/**
+	 * Skapar en string som blir ordet som ska vara bakvänt och kallar på reverse
+	 * metoden
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		String ord = "Hejsan";
+		System.out.println(reverse(ord));
+
+	}
+
+	/**
+	 * Placerar ut charen längst bak i string längst fram i temp och kollar sedan
+	 * ifall count är lika med längden på stringen och då är ordet bakvänt och
+	 * returnar ordet
+	 * 
+	 * @param s ordet som ska reversas
+	 * @return
+	 */
+	public static String reverse(String s) {
+		temp += s.charAt(s.length() - count - 1);
+		count++;
+		if (count >= s.length()) {
+			return s;
+		}
+		reverse(s);
+		return temp;
+	}
+
+}
