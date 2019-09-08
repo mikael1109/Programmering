4b79b26a330da09d4726b1486a84e4659132a3c7
Mikael1109 <mikmen111@gmail.com>
2019-09-08 13:34:46 +0200

    uppgift 4 klar och börjat på uppgift 5

---
+++ V36/src/Box.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class Box{
+
+	public <T> Box (T... obj) {
+		ArrayList<T> list = new ArrayList<>();
+		for (int i = 0; i < obj.length; i++) {
+			list.add(obj[i]);
+		}
+	}
+	
+	public static void main(String[] args) {
+		
+	}
+	
+	public static void
+
+}
