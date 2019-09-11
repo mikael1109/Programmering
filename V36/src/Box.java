c23eb78e710072688d71b98bfcc214476c6c3674
Mikael1109 <mikmen111@gmail.com>
2019-09-11 11:37:45 +0200

    ändrade uppgift 4 och gjorde uppgift 5

--- V36/src/Box.java
+++ V36/src/Box.java
@@ -3,17 +3,50 @@ import java.util.Arrays;
 
 public class Box{
 
+	public Object[] list;
+	
 	public <T> Box (T... obj) {
-		ArrayList<T> list = new ArrayList<>();
-		for (int i = 0; i < obj.length; i++) {
-			list.add(obj[i]);
+		list = new Object[obj.length];
+		for(int i = 0; i < obj.length; i++) {
+			list[i] = obj[i];
 		}
 	}
 	
 	public static void main(String[] args) {
+		String hej = "hej";
+		int i = 9;
+		Box box1 = new Box(i);
+		Box box2 = new Box(hej);
+		System.out.println(Arrays.toString(box1.list));
+		System.out.println(Arrays.toString(box2.list));
+		comp(box1, box2);
 		
 	}
 	
-	public static void
+	public static void comp(Box box1, Box box2) {
+		for(int i = 0; i < box1.list.length; i++) {
+			if(box1.list[i] instanceof String) {
+				for(int y = 0; y < box2.list.length; y++) {
+					if(box2.list[y] instanceof String) {
+						System.out.println("Båda boxarna innehåller String");
+					}
+				}
+			}else if(box1.list[i] instanceof Integer) {
+				for(int y = 0; y < box2.list.length; y++) {
+					if(box2.list[y] instanceof Integer) {
+						System.out.println("Båda boxarna innehåller Integer");
+					}
+				}
+			}else if(box1.list[i] instanceof Character) {
+				for(int y = 0; y < box2.list.length; y++) {
+					if(box2.list[y] instanceof Character) {
+						System.out.println("Båda boxarna innehåller Character");
+					}
+				}
+			}else {
+				System.out.println("Inga av boxarna innehåller samma datatyp");
+			}
+		}
+	}
 
 }
