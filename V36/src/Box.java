5b4a1dfafa426cfbabb0b31955c2f9e1ffd358c9
Mikael1109 <mikmen111@gmail.com>
2019-09-11 12:03:01 +0200

    la till kommentarer på uppgift 4 och 5

--- V36/src/Box.java
+++ V36/src/Box.java
@@ -1,51 +1,76 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 
-public class Box{
+public class Box {
 
-	public Object[] list;
-	
-	public <T> Box (T... obj) {
+	public Object[] list; // Listan för objekten som kommer lagras i klassen
+
+	/**
+	 * Skapar en array för klassen och lägger in alla objekt i den
+	 * 
+	 * @param obj
+	 *            objekten som ska lagras i klassen
+	 */
+	public <T> Box(T... obj) {
 		list = new Object[obj.length];
-		for(int i = 0; i < obj.length; i++) {
+		for (int i = 0; i < obj.length; i++) {
 			list[i] = obj[i];
 		}
 	}
-	
+
+	/**
+	 * Skapar boxarna med olika värden lagrade i den och kallar sedan på comp med
+	 * boxarna
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		String hej = "hej";
 		int i = 9;
-		Box box1 = new Box(i);
-		Box box2 = new Box(hej);
-		System.out.println(Arrays.toString(box1.list));
-		System.out.println(Arrays.toString(box2.list));
+		long l = 20009944;
+		float f = 294;
+		double d = 0.93;
+		Box box1 = new Box(i, l, f, d);
+		Box box2 = new Box(hej, 'c', 9, f, l);
 		comp(box1, box2);
-		
+
 	}
-	
+
+	/**
+	 * Skapar en int count och kollar ifall boxarna innehåller samma datatyp ifall
+	 * de har de lägger de på en på count och skriver ut ett meddelande ifall count
+	 * 0 i slutet av for loopen så skriver den att boxarna inte hade samma datatyper
+	 * 
+	 * @param box1 första boxen som ska jämföras
+	 * @param box2 andra boxen som ska jämföras
+	 */
 	public static void comp(Box box1, Box box2) {
-		for(int i = 0; i < box1.list.length; i++) {
-			if(box1.list[i] instanceof String) {
-				for(int y = 0; y < box2.list.length; y++) {
-					if(box2.list[y] instanceof String) {
-						System.out.println("Båda boxarna innehåller String");
-					}
+		int count = 0;
+		for (int i = 0; i < box1.list.length; i++) {
+			for (int y = 0; y < box2.list.length; y++) {
+				if (box1.list[i] instanceof String && box2.list[y] instanceof String) {
+					System.out.println("Båda boxarna innehåller String");
+					count++;
+				} else if (box1.list[i] instanceof Integer && box2.list[y] instanceof Integer) {
+					System.out.println("Båda boxarna innehåller Integer");
+					count++;
+				} else if (box1.list[i] instanceof Character && box2.list[y] instanceof Character) {
+					System.out.println("Båda boxarna innehåller Character");
+					count++;
+				} else if (box1.list[i] instanceof Long && box2.list[y] instanceof Long) {
+					System.out.println("Båda boxarna innehåller Long");
+					count++;
+				} else if (box1.list[i] instanceof Float && box2.list[y] instanceof Float) {
+					System.out.println("Båda boxarna innehåller Float");
+					count++;
+				} else if (box1.list[i] instanceof Double && box2.list[y] instanceof Double) {
+					System.out.println("Båda boxarna innehåller Double");
+					count++;
 				}
-			}else if(box1.list[i] instanceof Integer) {
-				for(int y = 0; y < box2.list.length; y++) {
-					if(box2.list[y] instanceof Integer) {
-						System.out.println("Båda boxarna innehåller Integer");
-					}
-				}
-			}else if(box1.list[i] instanceof Character) {
-				for(int y = 0; y < box2.list.length; y++) {
-					if(box2.list[y] instanceof Character) {
-						System.out.println("Båda boxarna innehåller Character");
-					}
-				}
-			}else {
-				System.out.println("Inga av boxarna innehåller samma datatyp");
 			}
+		}
+		if (count == 0) {
+			System.out.println("Inga av boxarna innehåller samma datatyp");
 		}
 	}
 
