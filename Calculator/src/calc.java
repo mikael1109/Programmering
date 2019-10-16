0069bf6eaf32e7205cc12219c4a065c6283f9b8d
Mikael1109 <mikmen111@gmail.com>
2019-10-16 11:06:02 +0200

    la till mer knappar och ändrade koden för att få fram summan

--- Calculator/src/calc.java
+++ Calculator/src/calc.java
@@ -23,11 +23,11 @@ public class calc extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
-	
+
 	public void start(Stage sStage) {
-		
-		functions func = new functions(); 
-		
+
+		functions func = new functions();
+
 		GridPane layout = new GridPane();
 		Button ett = new Button();
 		ett.setText("1");
@@ -68,7 +68,16 @@ public class calc extends Application {
 		Button add = new Button();
 		add.setText("+");
 		add.setMinSize(120, 120);
-		
+		Button multi = new Button();
+		multi.setText("x");
+		multi.setMinSize(120, 120);
+		Button divide = new Button();
+		divide.setText("/");
+		divide.setMinSize(120, 120);
+		Button dot = new Button();
+		dot.setText(".");
+		dot.setMinSize(120, 120);
+
 		layout.setHgap(10);
 		layout.setVgap(10);
 		layout.setMinSize(500, 500);
@@ -79,82 +88,121 @@ public class calc extends Application {
 		layout.add(fyra, 0, 2);
 		layout.add(fem, 1, 2);
 		layout.add(sex, 2, 2);
+		layout.add(multi, 3, 2);
 		layout.add(sju, 0, 3);
 		layout.add(atta, 1, 3);
 		layout.add(nio, 2, 3);
+		layout.add(divide, 3, 3);
 		layout.add(clear, 0, 4);
 		layout.add(noll, 1, 4);
 		layout.add(exe, 2, 4);
+		layout.add(dot, 3, 4);
 		layout.setAlignment(Pos.CENTER);
-		
+
+		TextField topField = new TextField();
+		topField.setMaxSize(500, 25);
+		topField.setMinSize(500, 25);
+		topField.setFont(Font.font("Verdana", FontWeight.BOLD, 15));
+		topField.setEditable(false);
+
 		TextField field = new TextField();
-		field.setMinWidth(500);
-		field.setMaxWidth(500);
-		field.setMinHeight(75);
-		field.setMaxHeight(75);
+		field.setMaxSize(500, 55);
+		field.setMinSize(500, 55);
 		field.setFont(Font.font("Verdana", FontWeight.BOLD, 30));
 		field.setEditable(false);
-		
+
+		BorderPane fieldBorder = new BorderPane();
+		fieldBorder.setTop(topField);
+		fieldBorder.setCenter(field);
+
 		BorderPane border = new BorderPane();
-		border.setTop(field);
+		border.setTop(fieldBorder);
 		border.setCenter(layout);
 		border.setAlignment(field, Pos.CENTER);
-		
+		border.setAlignment(topField, Pos.CENTER);
+
 		Scene mainScene = new Scene(border, 600, 600);
 		sStage.setScene(mainScene);
 		sStage.setTitle("Calculator");
 		sStage.setResizable(false);
 		sStage.show();
-		
+
 		ett.setOnAction(event -> {
-			func.actNumb(1);
+			func.actNumb('1');
 			field.setText(func.activeNumb);
 		});
 		tva.setOnAction(event -> {
-			func.actNumb(2);
+			func.actNumb('2');
 			field.setText(func.activeNumb);
 		});
 		tre.setOnAction(event -> {
-			func.actNumb(3);
+			func.actNumb('3');
 			field.setText(func.activeNumb);
 		});
 		fyra.setOnAction(event -> {
-			func.actNumb(4);
+			func.actNumb('4');
 			field.setText(func.activeNumb);
 		});
 		fem.setOnAction(event -> {
-			func.actNumb(5);
+			func.actNumb('5');
 			field.setText(func.activeNumb);
 		});
 		sex.setOnAction(event -> {
-			func.actNumb(6);
+			func.actNumb('6');
 			field.setText(func.activeNumb);
 		});
 		sju.setOnAction(event -> {
-			func.actNumb(7);
+			func.actNumb('7');
 			field.setText(func.activeNumb);
 		});
 		atta.setOnAction(event -> {
-			func.actNumb(8);
+			func.actNumb('8');
 			field.setText(func.activeNumb);
 		});
 		nio.setOnAction(event -> {
-			func.actNumb(9);
+			func.actNumb('9');
 			field.setText(func.activeNumb);
 		});
 		noll.setOnAction(event -> {
-			func.actNumb(0);
+			func.actNumb('0');
+			field.setText(func.activeNumb);
+		});
+		dot.setOnAction(event -> {
+			func.actNumb('.');
 			field.setText(func.activeNumb);
 		});
 		clear.setOnAction(event -> {
 			func.clear();
 			field.setText(func.activeNumb);
+			topField.setText(func.numb);
 		});
 		add.setOnAction(event -> {
-			
+			func.addNumb();
+			field.setText(func.activeNumb);
+			topField.setText(func.numb);
 		});
-		
-		
+		multi.setOnAction(event -> {
+			func.multiply();
+			field.setText(func.activeNumb);
+			topField.setText(func.numb);
+		});
+		divide.setOnAction(event -> {
+			func.divide();
+			field.setText(func.activeNumb);
+			topField.setText(func.numb);
+		});
+		exe.setOnAction(event -> {
+			func.execute();
+			if (func.sum % 1 == 0) {
+				field.setText(Integer.toString((int) func.sum));
+			} else {
+				field.setText(Double.toString(func.sum));
+			}
+			topField.setText(func.numb);
+			func.sum = 0;
+			func.numb = "";
+		});
+
 	}
 
 }
--- Calculator/src/functions.java
+++ Calculator/src/functions.java
@@ -1,25 +1,117 @@
-
 public class functions {
-	
+
 	public String activeNumb = "";
-	public int prevNumb;
-	public int activeFunc;
+	public String numb = "";
+	public double sum;
 	
-	public void functions(){
-		actNumb(0);
-		clear();
+	public static void main(String[] args) {
+		double i = 1.0/2.0;
+		System.out.println(i);
 	}
-	
+
 	public void clear() {
 		activeNumb = "";
+		numb = "";
 	}
-	
-	public void actNumb(int i) {
-		activeNumb += Integer.toString(i);
+
+	public void actNumb(char c) {
+		activeNumb += c;
 	}
-	
+
 	public void addNumb() {
-		activeFunc = 1;
+		if (activeNumb.equals("")) {
+
+		} else {
+			numb += (activeNumb + "+");
+			activeNumb = "";
+		}
+	}
+
+	public void multiply() {
+		if (activeNumb.equals("")) {
+
+		} else {
+			numb += (activeNumb + "x");
+			activeNumb = "";
+		}
+	}
+	public void divide() {
+		if (activeNumb.equals("")) {
+
+		} else {
+			numb += (activeNumb + "/");
+			activeNumb = "";
+		}
+	}
+
+	public void execute() {
+		int count = 0;
+		int maxCount = 0;
+		int prevFunc = 0;
+		numb += activeNumb;
+		System.out.println(numb);
+		char[] c = numb.toCharArray();
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == 'x' || c[i] == '+' || c[i] == '/') {
+				maxCount++;
+			}
+		}
+		for (int i = 0; i < c.length; i++) {
+			if (c[i] == '+' || c[i] == 'x' || c[i] == '/') {
+				if (count == 0) {
+					String firstNumb = "";
+					for (int y = 0; y < i; y++) {
+						if (c[y] == 'x' || c[y] == '+' || c[y] == '/') {
+
+						} else {
+							firstNumb += c[y];
+						}
+					}
+					sum = Double.parseDouble(firstNumb);
+				}
+				if(count > 0 || count != maxCount){
+					String aNumb = "";
+					for (int y = prevFunc; y < i; y++) {
+						if (c[y] == 'x' || c[y] == '+' || c[y] == '/') {
+
+						} else {
+							aNumb += c[y];
+						}
+					}
+					if(c[prevFunc] == '+') {
+						sum += Double.parseDouble(aNumb);
+					}else if(c[prevFunc] == 'x') {
+						sum *= Double.parseDouble(aNumb);
+					}else if(c[prevFunc] == '/') {
+						sum = sum/Double.parseDouble(aNumb);
+					}
+				}
+				count++;
+				prevFunc = i;
+				if(count == maxCount) {
+					String aNumb = "";
+					for (int y = prevFunc; y < c.length; y++) {
+						if (c[y] == 'x' || c[y] == '+' || c[y] == '/') {
+
+						} else {
+							aNumb += c[y];
+						}
+					}
+					if(c[prevFunc] == '+') {
+						sum += Double.parseDouble(aNumb);
+					}else if(c[prevFunc] == 'x') {
+						sum *= Double.parseDouble(aNumb);
+					}else if(c[prevFunc] == '/') {
+						sum = sum/Double.parseDouble(aNumb);
+					}
+					if(sum % 1 == 0) {
+						activeNumb = Integer.toString((int) sum);
+					}else {
+						activeNumb = Double.toString(sum);
+					}
+				}
+			}
+		}
 	}
 
 }
