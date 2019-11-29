2068e1c33ff50183d8087f3574b8ec487d89530a
Mikael1109 <mikmen111@gmail.com>
2019-11-29 12:21:53 +0100

    la till en connect check

--- El-Scooter-Projekt/src/Sensors.java
+++ El-Scooter-Projekt/src/Sensors.java
@@ -3,12 +3,13 @@ import java.util.ArrayList;
 
 public class Sensors {
 	
+	ArrayList<Sensor> sensorArray = new ArrayList<Sensor>();
 	public boolean sensorsActive = true;
+	public boolean sensorConnected = true;
 	int sensorsQuantity = 4;
-	int startCount = 1;
+	int sensorCount = 1;
 	
 	public void Sensors() throws InterruptedException{
-		ArrayList<Sensor> sensorArray = new ArrayList<Sensor>();
 		for (int i = 0; i < sensorsQuantity; i++) {
 			sensorArray.add(new Sensor());
 		}
@@ -20,18 +21,40 @@ public class Sensors {
 		if(sensorsActive) {
 			System.out.println("All sensors started");
 		}
+		sensorCount = 1;
+		for (int i = 0; i < sensorArray.size(); i++) {
+			if(!checkConnection(sensorArray.get(i))) {
+				sensorConnected = false;
+			}
+		}
+		if(sensorConnected) {
+			System.out.println("All sensors connected");
+		}
 	}
 	
 	
-	public void checkConnection(Sensor[] sensors){
-		
+	public boolean checkConnection(Sensor sensor) throws InterruptedException{
+		if(sensor.isConnected) {
+			connectTimer(sensor, sensorCount);
+			sensorCount++;
+			return true;
+		}else {
+			return false;
+		}
+	}
+	
+	public void connectTimer(Sensor sensor, int count) throws InterruptedException {
+		synchronized (sensor) {
+			sensor.wait(2000);
+			System.out.println(sensor + "(Sensor" + count + ")" + " has connected");
+		}
 	}
 	
 	
 	public boolean sensorStarted(Sensor sensor) throws InterruptedException {
 		if(sensor.active) {
-			startTimer(sensor, startCount);
-			startCount++;
+			startTimer(sensor, sensorCount);
+			sensorCount++;
 			return true;
 		}else {
 			return false;	
@@ -42,7 +65,7 @@ public class Sensors {
 	public void startTimer(Sensor sensor, int count) throws InterruptedException {
 		synchronized (sensor) {
 			sensor.wait(1000);
-			System.out.println("Sensor" + count + " has started");
+			System.out.println(sensor + "(Sensor" + count + ")" + " has started");
 		}
 	}
 
