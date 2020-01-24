f7d816ce6d34ed7d46205d9dafe13d3c679ae6f5
Mikael1109 <mikmen111@gmail.com>
2020-01-24 10:53:48 +0100

    la till en app class och ett lås på scootern

--- El-Scooter-Projekt/src/Scooter.java
+++ El-Scooter-Projekt/src/Scooter.java
@@ -1,9 +1,15 @@
 import java.io.IOException;
 
 public class Scooter {
+	
+	public boolean lock = true;
 
 	public static void main(String[] args) throws InterruptedException, NumberFormatException, IOException {
 		Helmet helmet = new Helmet();
+	}
+	
+	public void unlock() {
+		
 	}
 
 }
