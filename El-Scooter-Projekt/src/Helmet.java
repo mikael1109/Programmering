c852cbf5e87b65077f83f58bcf209ef8a1544815
mikael1109 <mikael.menonen@telia.com>
2020-03-05 18:31:24 +0100

    gjorde så rangechecken är gjord smartare

--- El-Scooter-Projekt/src/Helmet.java
+++ El-Scooter-Projekt/src/Helmet.java
@@ -1,19 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Helmet {
 
+	boolean[] localRangeList = new boolean[4];
 	public boolean sensorsInRange = true;
 	Sensors sensors = new Sensors();
 	boolean inRange = false;
 	boolean rangeCheck = false;
 	int sensorCount = 0;
-
-	boolean sensor1 = false;
-	boolean sensor2 = false;
-	boolean sensor3 = false;
-	boolean sensor4 = false;
 
 	public Helmet() throws InterruptedException, NumberFormatException, IOException {
 		
@@ -52,31 +49,13 @@ public class Helmet {
 	}
 
 	public void sysRange(boolean range) {
-		if (sensorCount == 0 && range) {
-			sensor1 = true;
-		} else if (sensorCount == 0 && !range) {
-			sensor1 = false;
+		if(range) {
+			localRangeList[sensorCount] = true;
+		}else if(!range) {
+			localRangeList[sensorCount] = false;
 		}
-
-		if (sensorCount == 1 && range) {
-			sensor2 = true;
-		} else if (sensorCount == 1 && !range) {
-			sensor2 = false;
-		}
-
-		if (sensorCount == 2 && range) {
-			sensor3 = true;
-		} else if (sensorCount == 2 && !range) {
-			sensor3 = false;
-		}
-
-		if (sensorCount == 3 && range) {
-			sensor4 = true;
-		} else if (sensorCount == 3 && !range) {
-			sensor4 = false;
-		}
-
-		if (sensor1 && sensor2 && sensor3 && sensor4 && !rangeCheck) {
+		
+		if(allRangeCheck(localRangeList) && !rangeCheck) {
 			System.out.println("All sensors in range");
 			rangeCheck = true;
 			inRange = true;
@@ -90,6 +69,15 @@ public class Helmet {
 			rangeCheck = false;
 		}
 
+	}
+	
+	public boolean allRangeCheck(boolean[] list) {
+		for(int i = 0; i < list.length; i++) {
+			if(!list[i]) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }
