8bcde60ba52d479e644a4009f460282ccd59804f
mikael1109 <mikael.menonen@telia.com>
2020-03-26 13:57:19 +0100

    La till så man kan ändra storlek på pensel

--- JavaFXDemo2/src/sample/Controller.java
+++ JavaFXDemo2/src/sample/Controller.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.ColorPicker;
+import javafx.scene.control.TextField;
 
 public class Controller {
 
@@ -14,10 +15,14 @@ public class Controller {
     @FXML
     ColorPicker _colorPicker;
 
+    @FXML
+    TextField _brushSize;
+    double size = 18;
+
     public void initialize() {
         GraphicsContext graphicsContext = _canvas.getGraphicsContext2D();
         _canvas.setOnMouseDragged(event -> {
-            double size = 18;
+
             double x = event.getX() - size / 2;
             double y = event.getY() - size / 2;
 
@@ -28,5 +33,15 @@ public class Controller {
 
     public void onExitClick(ActionEvent actionEvent) {
         System.exit(0);
+    }
+
+    public void changeSize(ActionEvent actionEvent) {
+        try{
+            Double.valueOf(_brushSize.getText());
+            size = Double.valueOf(_brushSize.getText());
+            initialize();
+        }catch (Exception e){
+            System.out.println("Not size value");
+        }
     }
 }
